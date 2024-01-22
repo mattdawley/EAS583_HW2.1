7f3699387088e7d8af70e9b3fd2e985897ced25f
Matt Dawley <dawley@gmail.com>
2024-01-22 06:57:41 -0700

    first attempt

--- sign.py
+++ sign.py
@@ -6,15 +6,14 @@ from eth_account.messages import encode_defunct
 def sign(m):
     w3 = Web3()
     # create an eth account and recover the address (derived from the public key) and private key
-    # your code here
+    acct = eth_account.Account.create()
 
-    eth_address = None  # Eth account
-    private_key = None
+
+    eth_address = acct.address  # Eth account
+    private_key = acct.key
 
     # generate signature
-    # your code here
-
-    signed_message = None
+    signed_message = eth_account.sign_message(m, private_key = private_key)
 
     assert isinstance(signed_message, eth_account.datastructures.SignedMessage)
 
